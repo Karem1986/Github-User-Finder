67baec4e2b6a6c67468b034c2a5871f7336176ef
Karem <karem.ortiz@outlook.com>
2020-05-21 11:25:35 +0200

    'app.js'

---
+++ app.js
@@ -0,0 +1,44 @@
+//Instantiate the class we created on github.js
+
+const github = new Github;
+
+//Instantiate the UI
+const ui = new UI; 
+    
+//Search input
+
+const searchUser = document.getElementById('searchUser');
+
+//Search input event listener 
+
+searchUser.addEventListener('keyup', (e) => {
+    //Get input text
+
+    const userText = e.target.value;
+
+    if(userText !== ''){
+        //Test it with console.log('')
+       //Make http call 
+       github.getUser(userText)
+         .then(data => {
+          
+            if(data.profile.message === 'Not Found') {
+                //Show alert//
+                ui.showAlert('User not found', 'alert alert-danger');
+                
+            } else {
+
+               //Show profile
+               ui.showProfile(data.profile); 
+               ui.showRepos(data.repos);
+
+            } 
+
+         })
+
+        } else {
+           //Clear profile 
+           ui.clearProfile();
+        }
+
+    }); 
